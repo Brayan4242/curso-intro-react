8dfe410a4d0c3b0413c22dd5a9acc7e935997912
Brayan4242 <dreyparker493@gmail.com>
2022-02-20 16:29:19 -0500

    clase 13

--- src/App/index.js
+++ src/App/index.js
@@ -7,19 +7,33 @@ import { AppUI } from "./AppUI";
 //   { text: 'Llorar con la llorona', completed: true },
 //   { text: 'LALALALAA', completed: false },
 // ];
+function useLocalStorage(itemName, initialValue) {
+
+    const localStorageItem = localStorage.getItem(itemName);
+    let parsedItem;
+
+    if (!localStorageItem) {
+        localStorage.setItem(itemName, JSON.stringify(initialValue));
+        parsedItem = initialValue;
+    } else {
+        parsedItem = JSON.parse(localStorageItem);
+    }
+    const [item, setItem] = React.useState(parsedItem);
+    const saveItem = (newItem) => {
+        const stringifiedItem = JSON.stringify(newItem);
+        localStorage.setItem(itemName, stringifiedItem);
+        setItem(newItem);
+    };
+    return [
+        item, saveItem,
+    ]
+
+}
 
 function App() {
-    const localStorageTodos = localStorage.getItem("TODOS_V1");
-    let parsedTodos;
+    const [todos, saveTodos] = useLocalStorage('TODOS_V1', [])
 
-    if (!localStorageTodos) {
-        localStorage.setItem("TODOS_V1", JSON.stringify([]));
-        parsedTodos = [];
-    } else {
-        parsedTodos = JSON.parse(localStorageTodos);
-    }
 
-    const [todos, setTodos] = React.useState(parsedTodos);
     const [searchValue, setSearchValue] = React.useState("");
 
     const completedTodos = todos.filter((todo) => !!todo.completed).length;
@@ -35,11 +49,7 @@ function App() {
         });
     }
 
-    const saveTodos = (newTodos) => {
-        const stringifiedTodos = JSON.stringify(newTodos);
-        localStorage.setItem("TODOS_V1", stringifiedTodos);
-        setTodos(newTodos);
-    };
+
 
     const completeTodo = (text) => {
         const todoIndex = todos.findIndex((todo) => todo.text === text);
